2ea7eb139deae1493ef80541229d7b5da583a70e
unknown <guneshprasads2506@gmail.com>
2022-12-04 18:47:34 +0530

    Attribute Annotation or Overriding Conventions

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace Vidly_2nd_try.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
