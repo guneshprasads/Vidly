e5389e7389554e15a2a83fbbb73796bb282e20e1
unknown <guneshprasads2506@gmail.com>
2022-12-05 14:39:43 +0530

    Getting the data from the database and Quering the objects and EAGER LOADING

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,37 +11,51 @@ namespace Vidly_2nd_try.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
-        
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public CustomerController()
         {
-            var customer = new List<Customer>
-            {
-                new Customer{ Id=1,Name = "Customer 1" },
-                new Customer{ Id=2,Name = "Customer 2" },
-                new Customer{ Id=3,Name = "Customer 3" }
-            };
-            return View(customer);
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Index() 
+        {
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
+
+            //var customer = new List<Customer>
+            //{
+            //    new Customer{ Id=1,Name = "Customer 1" },
+            //    new Customer{ Id=2,Name = "Customer 2" },
+            //    new Customer{ Id=3,Name = "Customer 3" }
+            //};
+            return View(customers);
         }
 
         
         public ActionResult Details(int id)
         {
-            if (id == 1)
-            {
-                var customer = new Customer() { Name = "Customer 1" };
-                return View(customer);
-            }
-            if (id == 2)
-            {
-                var customer = new Customer() { Name = "Customer 2" };
-                return View(customer);
-            }
-            if (id == 3)
-            {
-                var customer = new Customer() { Name = "Customer 3" };
-                return View(customer);
-            }
-            return Content("Something went wrong");
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            //if (id == 1)
+            //{
+            //    var customer = new Customer() { Name = "Customer 1" };
+            //    return View(customer);
+            //}
+            //if (id == 2)
+            //{
+            //    var customer = new Customer() { Name = "Customer 2" };
+            //    return View(customer);
+            //}
+            //if (id == 3)
+            //{
+            //    var customer = new Customer() { Name = "Customer 3" };
+            //    return View(customer);
+            //}
+            return View(customer);
         }
     }
 }
