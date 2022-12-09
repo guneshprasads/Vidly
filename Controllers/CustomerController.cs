f61c1718b51a8900df36cc04158c9daa3985db1b
unknown <guneshprasads2506@gmail.com>
2022-12-09 23:11:58 +0530

    Removed all the comments

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -26,13 +26,6 @@ namespace Vidly_2nd_try.Controllers
         public ActionResult Index() 
         {
             var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
-
-            //var customer = new List<Customer>
-            //{
-            //    new Customer{ Id=1,Name = "Customer 1" },
-            //    new Customer{ Id=2,Name = "Customer 2" },
-            //    new Customer{ Id=3,Name = "Customer 3" }
-            //};
             return View(customers);
         }
 
@@ -40,21 +33,6 @@ namespace Vidly_2nd_try.Controllers
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
-            //if (id == 1)
-            //{
-            //    var customer = new Customer() { Name = "Customer 1" };
-            //    return View(customer);
-            //}
-            //if (id == 2)
-            //{
-            //    var customer = new Customer() { Name = "Customer 2" };
-            //    return View(customer);
-            //}
-            //if (id == 3)
-            //{
-            //    var customer = new Customer() { Name = "Customer 3" };
-            //    return View(customer);
-            //}
             return View(customer);
         }
     }
